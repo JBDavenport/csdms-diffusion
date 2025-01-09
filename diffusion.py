44245d1e6019717a0eea0930012c467ed480c016
Jered Davenport <dnf44@txstate.edu>
2025-01-09 17:08:35 +0000

    Complete modularization of model script

--- diffusion.py
+++ diffusion.py
@@ -1,44 +1,52 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-def calculate_time_step(grid_spacing, diffusivity):
-    return 0.5 * grid_spacing**2 / diffusivity
+def calculate_time_step(GRID_SPACING, DIFFUSIVITY):
+    return 0.5 * GRID_SPACING**2 / DIFFUSIVITY
+
 
 
 def set_initial_profile(grid_size=100, boundary_left=500, boundary_right=0):
     profile = np.empty(grid_size)
-    profile[:grid_size//2] = boundary_left
-    profile[:grid_size//2:] = boundary_right
+    profile[: grid_size//2] = boundary_left
+    profile[grid_size//2 :] = boundary_right
     return profile
+
+
+def make_grid(origin, domain_size, grid_spacing):
+    grid = np.arange(start=origin, stop=origin+domain_size, step=grid_spacing)
+    return (grid, len(grid))
+
+
+def plot_profile(grid, concentration, color="r", title="Concentration Profile"):
+    plt.figure()
+    plt.plot(grid, concentration, color)
+    plt.xlabel("x")
+    plt.ylabel("C")
+    plt.title(title)
+
+
+def solve_1d_diffusion(concentration, grid_spacing=1.0, time_step=1.0, diffusivity=1.0):
+    centered_difference = np.roll(concentration, -1) - 2*concentration + np.roll(concentration, 1)
+    concentration[1:-1] = diffusivity * time_step / grid_spacing**2 * centered_difference[1:-1]
     
 
-D = 100 # diffusivity
-Lx = 300 # Domain size
+def diffusion_model():
+    D = 100 # diffusivity
+    Lx = 300 # Domain size
+    dx = 0.5
+    C_left = 500
+    C_right = 0
+    nt = 5000
+    
+    x, nx = make_grid(0.0, Lx, dx)
+    dt = calculate_time_step(dx, D)
+    C = set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
 
-dx = 0.5
-x = np.arange(start=0, stop=Lx, step=dx)
-nx = len(x)
+    plot_profile(x, C, title="Initial Concentration Profile")
 
-C_left = 500
-C_right = 0
-C = set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial concentration profile")
+    for t in range(0, nt):
+        solve_1d_diffusion(C, dx, dt, D)
 
-time = 0
-nt = 5000 #number of time steps
-dt = calculate_time_step(dx, D)
-
-for t in range(0, nt):
-    C += D * dt / dx**2 * (np.roll(C, -1) - 2*C + np.roll(C, 1))
-    C [0] = C_left
-    C[-1] = C_right
-plt.figure()
-plt.plot(x, C, "b") #"b" is blue color
-plt.xlabel("X")
-plt.ylabel("C")
-plt.title("Final concentration profile")
+    plot_profile(x, C, color="b", title="Final Concentration Profile")
