7408e6db051afcebff94eea8150d8fb90ecf4efd
Jered Davenport <dnf44@txstate.edu>
2025-01-09 15:40:51 +0000

    diffusion.py

---
+++ diffusion.py
@@ -0,0 +1,44 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def calculate_time_step(grid_spacing, diffusivity):
+    return 0.5 * grid_spacing**2 / diffusivity
+
+
+def set_initial_profile(grid_size=100, boundary_left=500, boundary_right=0):
+    profile = np.empty(grid_size)
+    profile[:grid_size//2] = boundary_left
+    profile[:grid_size//2:] = boundary_right
+    return profile
+    
+
+D = 100 # diffusivity
+Lx = 300 # Domain size
+
+dx = 0.5
+x = np.arange(start=0, stop=Lx, step=dx)
+nx = len(x)
+
+C_left = 500
+C_right = 0
+C = set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
+
+plt.figure()
+plt.plot(x, C, "r")
+plt.xlabel("x")
+plt.ylabel("C")
+plt.title("Initial concentration profile")
+
+time = 0
+nt = 5000 #number of time steps
+dt = calculate_time_step(dx, D)
+
+for t in range(0, nt):
+    C += D * dt / dx**2 * (np.roll(C, -1) - 2*C + np.roll(C, 1))
+    C [0] = C_left
+    C[-1] = C_right
+plt.figure()
+plt.plot(x, C, "b") #"b" is blue color
+plt.xlabel("X")
+plt.ylabel("C")
+plt.title("Final concentration profile")
